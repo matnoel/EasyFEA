3b349f18d1ca78c30ff9bdbd177ea22edf98646a
MatthieuNOEL <matthieu.noel7@gmail.com>
2022-01-03 10:31:49 +0100

    Sauvegarde avant optimisation calcul maillage

--- Etude2D.py
+++ Etude2D.py
@@ -14,7 +14,7 @@ from classes.Simu import Simu
 import numpy as np
 import matplotlib.pyplot as plt
 
-# os.system("cls")    #nettoie le terminal
+os.system("cls")    #nettoie le terminal
 
 # Data --------------------------------------------------------------------------------------------
 
@@ -30,8 +30,8 @@ b = 13
 P = -800 #N
 
 # Paramètres maillage
-typeElement = ModelGmsh.get_typesMaillage2D()[1]
-taille = h/10
+typeElement = ModelGmsh.get_typesMaillage2D()[0]
+taille = h/20
 
 # Materiau
 materiau = Materiau(dim)
@@ -89,6 +89,5 @@ if plotResult:
         plt.show()
 
 Affichage.NouvelleSection("Fin du programme")
-d = 1
 
 # %%
