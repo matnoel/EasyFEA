cedc6089613fd6613177b59e51ac1e24e77ce6e9
Matthieu NOEL <matthieu.noel7@gmail.com>
2023-10-27 11:11:05 +0200

    Update Functions.py

--- codes/Phasefield/Identif/Functions.py
+++ codes/Phasefield/Identif/Functions.py
@@ -7,9 +7,10 @@ import numpy as np
 import Materials
 from Interface_Gmsh import Interface_Gmsh, Mesh, ElemType
 from Geom import Point, Domain, Circle
+import Display
 
 
-folder_file = Folder.Get_Path(__file__)
+folder = Folder.Get_Path(__file__)
 
 # ----------------------------------------------
 # Datas
@@ -17,16 +18,16 @@ folder_file = Folder.Get_Path(__file__)
 
 # récupère les courbes forces déplacements
 # pathDataFrame = Folder.Join([folder_file, "data_dfEssais.pickle"])
-pathDataFrame = Folder.Join([folder_file, "data_dfEssaisRedim.pickle"])
+pathDataFrame = Folder.Join([folder, "data_dfEssaisRedim.pickle"])
 with open(pathDataFrame, "rb") as file:
     dfLoad = pd.DataFrame(pickle.load(file))
 
-pathDataLoadMax = Folder.Join([folder_file, "data_df_loadMax.pickle"])
+pathDataLoadMax = Folder.Join([folder, "data_df_loadMax.pickle"])
 with open(pathDataLoadMax, "rb") as file:
     dfLoadMax = pd.DataFrame(pickle.load(file))
 
 # récupère les proritétés identifiées
-pathParams = Folder.Join([folder_file, "params_Essais.xlsx"])
+pathParams = Folder.Join([folder, "params_Essais.xlsx"])
 # pathParams = Folder.Join([folder_file, "params_Essais new.xlsx"])
 dfParams = pd.read_excel(pathParams)
 
@@ -86,3 +87,77 @@ def Calc_a_b(forces, deplacements, fmax):
     a, b = vect_ab[0], vect_ab[1]
 
     return a, b
+
+
+
+if __name__ == "__main__":
+
+    plt = Display.plt
+
+    Display.Clear()
+
+    folderIden = Folder.Join([Folder.New_File("Essais FCBA",results=True), "Identification"])
+
+    pathData = Folder.Join([folderIden, "identification.xlsx"])
+
+    df = pd.read_excel(pathData)
+
+    df = df[(df['solveur']==1)&(df['ftol']==1e-5)]
+    df = df.sort_values(by=['Essai'])
+
+    df = df.set_index(np.arange(df.shape[0]))
+
+    # print(df)
+
+    # df.
+
+    ax_histo = plt.subplots()[1]
+    ax_histo.bar(df.index, df["f_crit"].values)
+    ax_histo.set_xticks(df.index)
+    ax_histo.set_xlabel("Samples")
+    ax_histo.set_ylabel("Crack initiation forces")
+    Display.Save_fig(folderIden, 'crack init essais')
+    # ax_histo.tick_params(axis='x', labelrotation = 45)
+    # plt.xlim([0, None])
+    # plt.ylim([0, y_max])
+
+    errors = [2,6,8,9,11,13,17]
+    # errors = []
+
+    df.drop(errors, axis=0, inplace=True)    
+
+
+    ax_fit = plt.subplots()[1]
+    ax_fit.set_xlabel('$G_c$')
+    ax_fit.set_ylabel('Crack initiation forces')
+
+    f_crit = df["f_crit"].values
+    Gc = df["Gc"].values
+    for i in range(Gc.size):        
+        ax_fit.scatter(Gc[i],f_crit[i],c='blue')
+        # ax.text(Gc[i],f_crit[i],f'Essai{i}')
+
+
+    from scipy.optimize import minimize
+    J = lambda x: np.linalg.norm(f_crit - (x[0]*Gc + x[1]))
+
+    res = minimize(J, [0,0])
+    a, b = tuple(res.x)
+
+    Gc_array = np.linspace(Gc.min(), Gc.max(), 100)
+    curve: np.ndarray = a*Gc_array + b
+    
+
+    r = np.mean((Gc-Gc.mean())/Gc.std() * (f_crit-f_crit.mean())/f_crit.std())
+    # r = np.corrcoef(Gc,f_crit)[0,1]
+
+
+    ax_fit.plot(Gc_array, curve,c='red')
+
+    ax_fit.text(Gc_array.mean(), curve.mean(), f"{a:.3f} Gc + {b:.3f}, r={r:.3f}", va='top')
+    # bbox=dict(boxstyle="square,pad=0.3",alpha=1,color='white')
+
+    Display.Save_fig(folderIden, "corr")
+
+
+    pass
