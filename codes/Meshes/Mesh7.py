3610ba3316447eb10dcbe83a974e12002cb556bf
Matthieu NOEL <matthieu.noel7@gmail.com>
2023-11-14 17:32:13 +0100

    new meshes

---
+++ codes/Meshes/Mesh7.py
@@ -0,0 +1,109 @@
+from Interface_Gmsh import gmsh, Interface_Gmsh, Point, Circle, Domain, ElemType, PointsList, Line
+import Display
+import Folder
+import numpy as np
+
+folder = Folder.Get_Path(__file__)
+
+if __name__ == '__main__':
+
+    Display.Clear()
+
+    # ----------------------------------------------
+    # Geom
+    # ----------------------------------------------
+    R = 10
+    e = 2
+    r = R-e
+    h = R * 2/3
+
+    meshSize = e/4
+
+    center = Point()
+
+    circle_ext = Circle(center, R*2, meshSize)
+    circle_int = Circle(center, r*2, meshSize, True)
+
+    useFillet = circle_int.isHollow
+    addCylinder = True
+    addBox = True
+    addRevolve = True
+
+    # ----------------------------------------------
+    # Mesh
+    # ----------------------------------------------
+
+    interface = Interface_Gmsh(True, True)
+    # dim, elemType = 2, ElemType.TRI3
+    dim, elemType = 3, ElemType.TETRA4
+
+    factory = interface._init_gmsh_factory('occ')
+
+    surfaces = interface._Surfaces(circle_ext, [circle_int])    
+    interface._Extrude(surfaces, [0,0,R], elemType)
+    vol1 = factory.getEntities(3)    
+
+    factory.synchronize()    
+
+    if useFillet:
+        surfs = gmsh.model.getBoundary(vol1)
+        lines = gmsh.model.getBoundary(surfs, False)        
+        gmsh.model.occ.fillet([vol1[0][1]], [abs(line[1]) for line in lines], [e/3])
+    
+    if addCylinder:
+        cylinder = factory.addCylinder(R+e,0,h,-2*R-e,0,0,e)
+        factory.cut(vol1, [(3, cylinder)])
+
+    if addBox:
+        box = factory.addBox(-R/4,-R-e,R/2, R/2, 2*R+e, R/2)
+        factory.cut(factory.getEntities(3), [(3, box)])
+
+    if addRevolve:
+        p1 = Point(R-e/2, 0, e, r=e/4)
+        p2 = Point(R, 0, e)
+        p3 = Point(R,0, e*4)
+        p4 = Point(R-e/2, 0, e*4, r=e/4)
+        contour = PointsList([p1, p2, p3, p4])    
+        surf = interface._Surfaces(contour, [])[0]
+
+        rev1 = factory.revolve([(2, surf)], 0,0,0,0,0,R,np.pi)
+        rev2 = factory.revolve([(2, surf)], 0,0,0,0,0,R,-np.pi)
+        factory.cut(factory.getEntities(3), [rev1[1], rev2[1]])
+
+    
+    factory.synchronize()
+
+    if meshSize > 0:
+        factory.mesh.setSize(factory.getEntities(0), meshSize)
+
+    interface._Set_PhysicalGroups(buildPoint=False, buildLine=True, buildSurface=True, buildVolume=False)
+    
+    interface._Meshing(dim, elemType)
+
+    mesh = interface._Construct_Mesh()
+
+    print(mesh)
+
+    if dim == 3:
+        print(f'volume = {mesh.volume:.3f}')
+
+    Display.Plot_Model(mesh)
+
+    Display.Plot_Mesh(mesh)
+
+    Display.plt.show()
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
---
+++ codes/Meshes/Mesh8.py
@@ -0,0 +1,108 @@
+from Interface_Gmsh import gmsh, Interface_Gmsh, Point, Circle, Domain, ElemType, PointsList, Line, CircleArc
+import Display
+import Folder
+import numpy as np
+
+folder = Folder.Get_Path(__file__)
+
+if __name__ == '__main__':
+
+    Display.Clear()
+
+    # ----------------------------------------------
+    # Geom
+    # ----------------------------------------------
+    H = 90
+    L = 45
+    D = 10
+    e = 20    
+
+    N = 5
+    mS = (np.pi/4 * D/2) / N
+
+    # PI for Points
+    # pi for gmsh points
+    PC = Point(0, H/2, 0)
+    circle = Circle(PC, D, mS)
+    
+    P1 = Point(-L/2,0)
+    P2 = Point(L/2,0)
+    P3 = Point(L/2,H)
+    P4 = Point(-L/2,H)
+    contour = PointsList([P1, P2, P3, P4])
+
+    # ----------------------------------------------
+    # Mesh
+    # ----------------------------------------------
+    interface = Interface_Gmsh(False, True)
+    dim, elemType = 2, ElemType.QUAD4
+
+    factory = gmsh.model.occ
+
+    # mesh = interface.Mesh_2D(contour, [circle], elemType)
+
+    # gmsh points for the domain
+    p1 = factory.addPoint(*P1.coordo, meshSize=mS)
+    p2 = factory.addPoint(*P2.coordo, meshSize=mS)
+    p3 = factory.addPoint(*P3.coordo, meshSize=mS)
+    p4 = factory.addPoint(*P4.coordo, meshSize=mS)
+    
+    # gmsh points for the circle
+    dx = np.cos(np.pi/4) * D/2
+    dy = np.sin(np.pi/4) * D/2
+    pc = factory.addPoint(*PC.coordo, meshSize=mS)
+    pc1 = factory.addPoint(*(PC+[D/2,0]).coordo, meshSize=mS)
+    pc2 = factory.addPoint(*(PC+[0,D/2]).coordo, meshSize=mS)
+    pc3 = factory.addPoint(*(PC+[-D/2,0]).coordo, meshSize=mS)
+    pc4 = factory.addPoint(*(PC+[0,-D/2]).coordo, meshSize=mS)
+    pc12 = factory.addPoint(*(PC+[dx,dy]).coordo, meshSize=mS)
+    pc23 = factory.addPoint(*(PC+[-dx,dy]).coordo, meshSize=mS)
+    pc34 = factory.addPoint(*(PC+[-dx,-dy]).coordo, meshSize=mS)
+    pc41 = factory.addPoint(*(PC+[dx,-dy]).coordo, meshSize=mS)
+
+    p23 = factory.addPoint(*(P2+P3).coordo/2, meshSize=mS)
+    p34 = factory.addPoint(*(P3+P4).coordo/2, meshSize=mS)
+    p41 = factory.addPoint(*(P4+P1).coordo/2, meshSize=mS)
+    p12 = factory.addPoint(*(P1+P2).coordo/2, meshSize=mS)
+
+    points_1 = [pc1, pc12, pc2, pc23, pc3, pc34, pc4, pc41]
+    points_2 = [p23, p3, p34, p4, p41, p1, p12, p2]
+    points_3 = [p3, p34, p4, p41, p1, p12, p2, p23]
+    points_4 = [pc12, pc2, pc23, pc3, pc34, pc4, pc41, pc1]
+
+    for p in range(len(points_1)):
+
+        if p == 0:
+            line1 = factory.addLine(points_1[p], points_2[p])
+        else:
+            line1 = line3
+        line2 = factory.addLine(points_2[p], points_3[p])
+        line3 = factory.addLine(points_3[p], points_4[p])
+        line4 = factory.addCircleArc(points_4[p], pc, points_1[p])
+
+        lines = [line1, line2, line3, line4]
+        loop = factory.addCurveLoop(lines)
+        surf = factory.addPlaneSurface([loop])
+
+        factory.synchronize()
+
+        [gmsh.model.mesh.setTransfiniteCurve(line, N) for line in lines]
+
+    factory.synchronize()
+
+    interface._Set_PhysicalGroups()
+    
+    interface._Meshing(dim, elemType, isOrganised=True)
+
+    mesh = interface._Construct_Mesh()
+
+    print(mesh)
+
+    if dim == 3:
+        print(f'volume = {mesh.volume:.3f}')
+        
+    Display.Plot_Model(mesh)
+
+    Display.Plot_Mesh(mesh)
+
+    Display.plt.show()
