a5bd97dac4108fabb6227444600b6bda7449c8f7
Matthieu NOEL <matthieu.noel7@gmail.com>
2023-11-17 13:07:07 +0100

    clarifies elasticity

---
+++ codes/Elasticity/Elas1.py
@@ -0,0 +1,75 @@
+"""Bending beam"""
+
+import Folder
+import Display
+import TicTac
+from Interface_Gmsh import Interface_Gmsh, ElemType, Point, PointsList, Circle, Domain
+import Simulations
+import Materials
+import matplotlib.pyplot as plt
+import numpy as np
+
+Display.Clear()
+
+# Define dimension and mesh size parameters
+dim = 2
+N = 20 if dim == 2 else 10
+
+
+# Define material properties
+E = 210000  # MPa (Young's modulus)
+v = 0.3     # Poisson's ratio
+coef = 1
+
+L = 120 # mm
+h = 13
+load = 800
+
+# --------------------------------------------------------------------------------------------
+# Mesh
+# --------------------------------------------------------------------------------------------
+
+meshSize = h/10
+
+domain = Domain(Point(), Point(L,h), meshSize)
+
+if dim == 2:
+    mesh = Interface_Gmsh().Mesh_2D(domain, [], ElemType.QUAD4, isOrganised=True)
+else:
+    mesh = Interface_Gmsh().Mesh_3D(domain, [], [0,0,-h], 4, ElemType.HEXA8, isOrganised=True)
+
+nodesX0 = mesh.Nodes_Conditions(lambda x, y, z: x == 0)
+nodesXL = mesh.Nodes_Conditions(lambda x, y, z: x == L)
+
+# --------------------------------------------------------------------------------------------
+# Simulation
+# --------------------------------------------------------------------------------------------
+
+material = Materials.Elas_Isot(dim, E, v, planeStress=True, thickness=h)
+simu = Simulations.Simu_Displacement(mesh, material)
+
+simu.add_dirichlet(nodesX0, [0]*dim, simu.Get_directions())
+# # is the same as
+# if dim == 2:
+#     simu.add_dirichlet(nodesX0, [0, 0], ["x", "y"])    
+# else:
+#     simu.add_dirichlet(nodesX0, [0, 0, 0], ["x", "y", "z"]) 
+
+simu.add_surfLoad(nodesXL, [-load/h**2], ["y"])
+
+sol = simu.Solve()
+simu.Save_Iter()
+
+# --------------------------------------------------------------------------------------------
+# Results
+# --------------------------------------------------------------------------------------------
+print(simu)
+
+Display.Plot_Model(mesh)
+Display.Plot_BoundaryConditions(simu)
+Display.Plot_Mesh(simu, h/2/np.abs(sol).max())
+Display.Plot_Result(simu, "Svm", nodeValues=True, coef=1/coef, nColors=20)
+
+TicTac.Tic.Plot_History(details=False)
+
+plt.show()
---
+++ codes/Elasticity/Elas3.py
@@ -0,0 +1,74 @@
+"""Hydraulic dam"""
+
+import Folder
+import Display
+import TicTac
+from Interface_Gmsh import Interface_Gmsh, ElemType, Point, PointsList, Circle, Domain
+import Simulations
+import Materials
+import matplotlib.pyplot as plt
+import numpy as np
+
+Display.Clear()
+
+# Define dimension and mesh size parameters
+dim = 2
+N = 20 if dim == 2 else 10
+
+coef = 1e6
+E = 15000*coef  # Pa (Young's modulus)
+v = 0.25          # Poisson's ratio
+
+g = 9.81   # m/s^2 (acceleration due to gravity)
+ro = 2400  # kg/m^3 (density)
+w = 1000   # kg/m^3 (density)
+
+h = 180  # m (thickness)
+thickness = 2*h
+
+# --------------------------------------------------------------------------------------------
+# Mesh
+# --------------------------------------------------------------------------------------------
+
+pt1 = Point()
+pt2 = Point(x=h)
+pt3 = Point(y=h)
+contour = PointsList([pt1, pt2, pt3], h/N)
+
+if dim == 2:
+    mesh = Interface_Gmsh().Mesh_2D(contour, [], ElemType.TRI6)
+    print(f"err area = {np.abs(mesh.area - h**2/2):.3e}")
+elif dim == 3:
+    mesh = Interface_Gmsh().Mesh_3D(contour, [], [0, 0, -thickness], 3, ElemType.PRISM6)
+    print(f"error volume = {np.abs(mesh.volume - h**2/2 * thickness):.3e}")
+
+nodesX0 = mesh.Nodes_Conditions(lambda x, y, z: x == 0)
+nodesY0 = mesh.Nodes_Conditions(lambda x, y, z: y == 0)
+
+# --------------------------------------------------------------------------------------------
+# Simulation
+# --------------------------------------------------------------------------------------------
+
+material = Materials.Elas_Isot(dim, E, v, planeStress=False, thickness=thickness)
+simu = Simulations.Simu_Displacement(mesh, material)
+
+simu.add_dirichlet(nodesY0, [0]*dim, simu.Get_directions())
+simu.add_surfLoad(nodesX0, [lambda x, y, z: w*g*(h - y)], ["x"], description="[w*g*(h-y)]")
+simu.add_volumeLoad(mesh.nodes, [-ro*g], ["y"], description="[-ro*g]")
+
+sol = simu.Solve()
+simu.Save_Iter()
+
+# --------------------------------------------------------------------------------------------
+# Results
+# --------------------------------------------------------------------------------------------
+print(simu)
+
+Display.Plot_Model(mesh)
+Display.Plot_BoundaryConditions(simu)
+Display.Plot_Mesh(simu, h/10/np.abs(sol.max()))
+Display.Plot_Result(simu, "Svm", nodeValues=True, coef=1/coef, nColors=20)
+
+TicTac.Tic.Plot_History(details=False)
+
+plt.show()
