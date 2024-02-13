51a53043668e6aacec3df70e7635ac2824bde887
Matthieu NOEL <matthieu.noel7@gmail.com>
2024-02-13 15:47:58 +0100

    Update GroupElem.py

--- modules/GroupElem.py
+++ modules/GroupElem.py
@@ -3,5 +3,5 @@ I considered renaming 'GroupElem.py' to 'GroupElems.py', but faced issues with l
 As a temporary solution, I've created this module to serve as a redirect, ensuring smooth access to the new module while maintaining compatibility with the old one. TODO: Delete this module at the end of the thesis.
 """
 from Display import myPrintError
-myPrintError("Link GroupElem.py to GroupElems.py\n(DO NOT USE IT)")
+myPrintError("Link GroupElem.py to GroupElems.py (DO NOT USE IT)")
 from GroupElems import *
