4db02bb7973a506405b40db5bc90abefe0661bc1
matnoel <matthieu.noel7@gmail.com>
2024-06-24 10:57:07 +0200

    Update Mesh10.py

--- examples/Meshes/Mesh10.py
+++ examples/Meshes/Mesh10.py
@@ -11,8 +11,6 @@ if __name__  == '__main__':
 
     Display.Clear()
 
-    dim = 3
-
     L = 1
     H = 2
     e = L * 0.5
@@ -31,19 +29,16 @@ if __name__  == '__main__':
     l4 = CircleArc(p4,p1,P=p6)
 
     contour = Contour([l1,l2,l3,l4])
-    contour2 = Domain(p1-[0,H/2], p2)
+    contour2 = Domain(p1-[0,H/2], p2, isHollow=False)
     contour3 = contour2.Copy(); contour3.Translate(dy=H+H/2)
 
-    surfaces = [(contour2, []), (contour3, [])]
+    surfaces = [contour2, contour3]
 
     ax = contour.Plot()
     contour2.Plot(ax)
     contour3.Plot(ax)
 
-    if dim == 2:
-        mesh = Mesher().Mesh_2D(contour, isOrganised=True, elemType='QUAD4', surfaces=surfaces)
-    else:        
-        mesh = Mesher().Mesh_Extrude(contour, [], [0,0,e], [3], isOrganised=True, elemType='HEXA8', surfaces=surfaces)
+    mesh = Mesher().Mesh_Extrude(contour, [], [0,0,e], [3], isOrganised=True, elemType='HEXA8', additionalSurfaces=surfaces)
 
     oldArea = mesh.area
     mesh.Rotate(-45, mesh.center)
