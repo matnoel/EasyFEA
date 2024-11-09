9fd450f49b8dcea26f74b424b5989a8b6f5c49ec
matnoel <matthieu.noel7@gmail.com>
2024-11-09 15:34:08 +0100

    Update Mesh12.py

--- examples/Meshes/Mesh12.py
+++ examples/Meshes/Mesh12.py
@@ -6,17 +6,19 @@
 
 from EasyFEA import Display, Folder, Mesher, ElemType
 
-folder = Folder.Get_Path(__file__) # Meshes folder
-examples_folder = Folder.Get_Path(folder)
+if __name__ == "__main__":
 
-stp = Folder.Join(examples_folder, "_parts", "beam.stp")
-mesh_stp = Mesher().Mesh_Import_part(stp, 3, 13/5, ElemType.TETRA4)
+    folder = Folder.Get_Path(__file__) # Meshes folder
+    examples_folder = Folder.Get_Path(folder)
 
-igs = Folder.Join(examples_folder, "_parts", "beam.igs")
-mesh_igs = Mesher().Mesh_Import_part(igs, 3, 13/5, ElemType.TETRA10)
-# An igs file will provide a contour mesh only.
+    stp = Folder.Join(examples_folder, "_parts", "beam.stp")
+    mesh_stp = Mesher().Mesh_Import_part(stp, 3, 13/5, ElemType.TETRA4)
 
-Display.Plot_Mesh(mesh_stp)
-Display.Plot_Mesh(mesh_igs)
+    igs = Folder.Join(examples_folder, "_parts", "beam.igs")
+    mesh_igs = Mesher().Mesh_Import_part(igs, 3, 13/5, ElemType.TETRA10)
+    # An igs file will provide a contour mesh only.
 
-Display.plt.show()
+    Display.Plot_Mesh(mesh_stp)
+    Display.Plot_Mesh(mesh_igs)
+
+    Display.plt.show()
