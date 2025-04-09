77c27fe2fa4cdda268979353044cfa938caaf493
matnoel <matthieu.noel7@gmail.com>
2025-04-09 16:28:30 +0200

    #6 Create HyperElasticLaws example

---
+++ examples/HyperElastic/HyperElasticLaws.py
@@ -0,0 +1,110 @@
+# Copyright (C) 2021-2025 Université Gustave Eiffel.
+# This file is part of the EasyFEA project.
+# EasyFEA is distributed under the terms of the GNU General Public License v3 or later, see LICENSE.txt and CREDITS.md for more information.
+
+from EasyFEA import Display
+
+try:
+    import sympy    
+except ModuleNotFoundError:
+    raise Exception("sympy must be installed!")
+
+def Compute(W, params: list):
+
+    W = sympy.sympify(W)
+
+    print(f"W = {W}\n")
+
+    # dW
+    dW = ""
+    for i, param_i in enumerate(params):        
+        dWdIi = sympy.diff(W, param_i)
+        if dWdIi != 0:
+            if i > 0:
+                dW += " + "
+            dW += f"{dWdIi} * dI{i+1}dC"
+    dW = dW.replace("+ -", "- ")
+    print(f"dW = 2 * ({dW})\n")
+
+    # d2W
+    d2W1 = ""
+    d2W2 = ""
+
+    for i, param_i in enumerate(params):        
+        dWdIi = sympy.diff(W, param_i)
+        if dWdIi != 0:
+            if i > 0:
+                d2W1 += " + "
+            d2W1 += f"{dWdIi} * d2I{i+1}dC"
+        for j, param_j in enumerate(params):             
+            d2WdIiIj = sympy.diff(dWdIi, param_j)
+            if d2WdIiIj != 0:
+                if j > 0:
+                    d2W2 += " + "
+                d2W2 += f"{d2WdIiIj} * dI{i+1}dC.T @ dI{j+1}dC"
+
+    d2W1 = d2W1.replace("+ -", "- ")
+    d2W2 = d2W2.replace("+ -", "- ")
+
+    if d2W2 == "":
+        print(f"d2W = 4 * ({d2W1})")
+    else:
+        print(f"d2W = 4 * ({d2W1}) + 4 * ({d2W2})")
+
+if __name__ == "__main__":
+
+    Display.Clear()
+
+    I1, I2, I3, I4 = sympy.symbols("I1, I2, I3, I4")
+    
+    # -------------------------------------
+    # Neo-Hookean
+    # -------------------------------------
+
+    Display.Section("Neo-Hookean")
+
+    K = sympy.symbols("K")
+    
+    W = K * (I1 - 3)
+
+    Compute(W, [I1])
+
+    # -------------------------------------
+    # Mooney-Rivlin
+    # -------------------------------------   
+
+    Display.Section("Mooney-Rivlin")
+
+    K1, K2 = sympy.symbols("K1, K2")
+    
+    W = K1 * (I1 - 3) + K2 * (I2 - 3)
+
+    Compute(W, [I1, I2])
+
+    # -------------------------------------
+    # Saint-Venant-Kirchhoff
+    # -------------------------------------   
+
+    Display.Section("Saint-Venant-Kirchhoff")
+
+    lmbda, mu = sympy.symbols("lmbda, mu")
+    
+    W = lmbda/8 * (I1**2 - 6*I1 + 9) + mu/4 * (I1**2 - 2*I1 - 2*I2 + 3)
+
+    Compute(W, [I1, I2, I3])
+
+    # -------------------------------------
+    # Ciarlet Geymonat
+    # -------------------------------------
+    
+    # seems strange
+
+    Display.Section("Ciarlet Geymonat")
+
+    J1 = I1 * I3**(sympy.Rational(-1,3))
+    J2 = I2 * I3**(sympy.Rational(-2,3))
+    J3 = I3**(sympy.Rational(1,2))
+    
+    W = K1 * (J1 - 3) + K2 * (J2 - 3) * K*((J3 - 1) - sympy.ln(J3))
+
+    Compute(W, [I1, I2, I3])
