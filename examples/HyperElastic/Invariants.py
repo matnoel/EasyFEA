716458cd929658c4cf21dd392d9c6e610b6d89f8
matnoel <matthieu.noel7@gmail.com>
2025-04-23 08:06:12 +0200

    #6 Update Invariants.py

--- examples/HyperElastic/Invariants.py
+++ examples/HyperElastic/Invariants.py
@@ -14,26 +14,13 @@ def __Project_Mandel(A, orderA:int=4):
     assert orderA in [2, 4]
     
     # for xx, yy, zz, yz, xz, zy
-    e = sympy.Array([ [0, 5, 4], [5, 1, 3], [4, 3, 2] ])
-
-    # # for xx, yy, zz, xy, xz, yz
-    # e = sympy.Array([ [0, 3, 4], [3, 1, 5], [4, 5, 2] ])
+    e = sympy.Array([
+        [0, 5, 4],
+        [5, 1, 3],
+        [4, 3, 2]
+    ])
 
     kron = lambda a, b: 1 if a == b else 0
-
-    # The use of a symmetrical matrix C on sympy is problematic here.
-    # The off-diagonal terms are incorrectly weighted and need to be corrected.
-    # The following function is used to correct the coefficients.
-    def get_coef(I: int, J: int):
-        if J == 0:
-            return 1/2 if I > 2 else 1
-        else:
-            if I > 2 and J > 2:
-                return 1/4
-            elif I > 2 or J > 2:
-                return 1/2
-            else:
-                return 1
 
     if orderA == 2:
         # Aij -> AI
@@ -41,8 +28,7 @@ def __Project_Mandel(A, orderA:int=4):
 
         for i in range(3):
             for j in range(3):
-                coef = get_coef(e[i,j], 0)
-                A_I[e[i,j]] = sympy.sqrt((2 - kron(i,j))) * A[i,j] * coef
+                A_I[e[i,j]] = sympy.sqrt((2 - kron(i,j))) * A[i,j]
 
         res = A_I
 
@@ -55,8 +41,7 @@ def __Project_Mandel(A, orderA:int=4):
             for j in range(3):
                 for k in range(3):
                     for l in range(3):
-                        coef = get_coef(e[i,j], e[k,l])
-                        A_IJ[e[i,j],e[k,l]] = sympy.sqrt((2-kron(i,j))*(2-kron(k,l))) * A[i,j,k,l] * coef
+                        A_IJ[e[i,j],e[k,l]] = sympy.sqrt((2-kron(i,j))*(2-kron(k,l))) * A[i,j,k,l]
 
         res = A_IJ
 
@@ -69,7 +54,33 @@ def __MyDiff(func, list_func: list, order:int=None):
 
     assert isinstance(list_func, list)
 
-    diff = sympy.diff(func, *list_func)
+    diff = sympy.diff(func, *list_func).as_mutable()
+
+    ndim = len(diff.shape)
+
+    e = sympy.Array([
+        [0, 5, 4],
+        [5, 1, 3],
+        [4, 3, 2]
+    ])
+
+    # The use of a symmetrical matrix C on sympy is problematic here.
+    # The off-diagonal terms are incorrectly weighted and need to be corrected.
+    # The following function is used to correct the coefficients.
+    if ndim == 2:
+        for i in range(diff.shape[0]):
+            for j in range(diff.shape[1]):
+                if e[i,j] > 2:
+                    diff[i,j] /= 2
+    elif ndim == 4:
+        for i in range(diff.shape[0]):
+            for j in range(diff.shape[1]):
+                for k in range(diff.shape[2]):
+                    for l in range(diff.shape[3]):
+                        if e[i,j] > 2 and e[k,l] > 2:
+                            diff[i,j,k,l] /= 4
+                        elif e[i,j] > 2 or e[k,l] > 2:
+                            diff[i,j,k,l] /= 2
 
     if order is not None:
         diff = __Project_Mandel(diff, order)
@@ -98,7 +109,6 @@ if __name__ == "__main__":
 
     Display.Clear()
 
-    # v1
     cxx, cyy, czz, cyz, cxz, cxy = sympy.symbols("cxx, cyy, czz, cyz, cxz, cxy")
     C = sympy.Matrix([
         [cxx, cxy, cxz],
@@ -107,6 +117,8 @@ if __name__ == "__main__":
     ])
 
     sympy.pprint(C)
+    print()
+    sympy.pprint(__Project_Mandel(C, 2))
     
     # -------------------------------------
     # I1
