7c0ca92649265f9346381a996ca7e84fd5a636be
matnoel <matthieu.noel7@gmail.com>
2025-07-09 19:52:49 +0200

    Update Dyna1.py

--- examples/Dynamic/Dyna1.py
+++ examples/Dynamic/Dyna1.py
@@ -17,6 +17,7 @@ from EasyFEA import (
     plt,
     ElemType,
     Simulations,
+    PyVista,
     Paraview,
 )
 from EasyFEA.Geoms import Domain
@@ -24,29 +25,18 @@ from EasyFEA.Geoms import Domain
 if __name__ == "__main__":
     Display.Clear()
 
+    makeParaview = False
+    makeMovie = True
+    plotIter = False
+
     # ----------------------------------------------
     # Configuration
     # ----------------------------------------------
     dim = 2
     folder = Folder.Join(Folder.RESULTS_DIR, "Dynamics", "Dyna1", mkdir=True)
-    plotResult = True
 
-    initSimu = True
-    depInit = -7
-    load = -800  # N
-
-    makeParaview = False
-    makeMovie = False
-    plotIter = True
+    depInit = -10
     resultToPlot = "uy"
-
-    # Dumping
-    # coefM = 1e-2
-    # coefK = 1e-2*2
-    coefM = 1e-3
-    coefK = 1e-3
-
-    tic_Tot = Tic()
 
     # geom
     L = 120
@@ -54,24 +44,28 @@ if __name__ == "__main__":
     h = 13
     b = 13
 
+    # time
+    Tmax = 0.5
+    N = 50
+    dt = Tmax / N
+    time = -dt
+
+    # Dumping
+    coefM = 1e-3
+    coefK = 1e-3
+
     # ----------------------------------------------
     # Mesh
     # ----------------------------------------------
     meshSize = h / 5
 
     if dim == 2:
-        # TRI3, TRI6, TRI10, TRI15, QUAD4, QUAD8, QUAD9
-        elemType = ElemType.QUAD4
         domain = Domain((0, -h / 2), (L, h / 2), meshSize)
-        mesh = domain.Mesh_2D(elemType=elemType, isOrganised=True)
+        mesh = domain.Mesh_2D([], ElemType.QUAD4, isOrganised=True)
         area = mesh.area - L * h
     elif dim == 3:
-        # TETRA4, TETRA10, HEXA8, HEXA20, HEXA27, PRISM6, PRISM15, PRISM18
-        elemType = ElemType.HEXA8
         domain = Domain((0, -h / 2, -b / 2), (L, h / 2, -b / 2), meshSize=meshSize)
-        mesh = domain.Mesh_Extrude(
-            [], [0, 0, b], elemType=elemType, layers=[3], isOrganised=True
-        )
+        mesh = domain.Mesh_Extrude([], [0, 0, b], [3], ElemType.HEXA8, isOrganised=True)
 
         volume = mesh.volume - L * b * h
         area = mesh.area - (L * h * 4 + 2 * b * h)
@@ -85,17 +79,20 @@ if __name__ == "__main__":
     # ----------------------------------------------
 
     material = Models.ElasIsot(dim, thickness=b)
-    simu = Simulations.ElasticSimu(mesh, material, useIterativeSolvers=initSimu)
+    simu = Simulations.ElasticSimu(mesh, material)
     simu.rho = 8100 * 1e-9
 
-    if initSimu:
-        simu.Bc_Init()
-        simu.add_dirichlet(nodes_0, [0] * dim, simu.Get_unknowns(), description="Fixed")
-        simu.add_dirichlet(nodes_L, [depInit], ["y"], description="dep")
-        simu.Solve()
-        simu.Save_Iter()
+    # static simulation
+    simu.Bc_Init()
+    simu.add_dirichlet(nodes_0, [0] * dim, simu.Get_unknowns(), description="Fixed")
+    simu.add_dirichlet(nodes_L, [depInit], ["y"], description="dep")
+    simu.Solve()
+    simu.Save_Iter()
+    Display.Plot_Mesh(simu, deformFactor=1)
 
-        Display.Plot_Mesh(simu, deformFactor=1)
+    # dynamic simulation
+    simu.Bc_Init()
+    simu.add_dirichlet(nodes_0, [0] * dim, simu.Get_unknowns(), description="Fixed")
 
     factorDef = 1
     if plotIter:
@@ -103,21 +100,12 @@ if __name__ == "__main__":
             simu, resultToPlot, nodeValues=True, plotMesh=True, deformFactor=factorDef
         )
 
-    Tmax = 0.5
-    N = 100
-    dt = Tmax / N
-    time = -dt
-
     simu.Solver_Set_Hyperbolic_Algorithm(dt)
     simu.Set_Rayleigh_Damping_Coefs(coefM=coefM, coefK=coefK)
 
     while time <= Tmax:
         time += dt
 
-        simu.Bc_Init()
-        simu.add_dirichlet(nodes_0, [0] * dim, simu.Get_unknowns(), description="Fixed")
-        if not initSimu:
-            simu.add_surfLoad(nodes_L, [load * time / Tmax / (h * b)], ["y"])
         simu.Solve()
         simu.Save_Iter()
 
@@ -134,38 +122,29 @@ if __name__ == "__main__":
 
         print(f"{time:.3f} s", end="\r")
 
-    tic_Tot.Tac("Time", "total time", True)
-
     # ----------------------------------------------
-    # Post processing
+    # Results
     # ----------------------------------------------
-    Display.Section("Post processing")
 
     Display.Plot_BoundaryConditions(simu)
-
-    # folder=""
 
     if makeParaview:
         Paraview.Save_simu(simu, folder)
 
     if makeMovie:
-        Display.Movie_Simu(
+        PyVista.Movie_simu(
             simu,
             resultToPlot,
             folder,
-            f"{resultToPlot}.mp4",
+            f"{resultToPlot}.gif",
             deformFactor=1,
-            plotMesh=True,
+            show_edges=True,
             N=400,
             nodeValues=True,
         )
 
-    if plotResult:
-        tic = Tic()
-        print(simu)
-        Display.Plot_Result(simu, "uy", deformFactor=factorDef, nodeValues=False)
-        Display.Plot_Result(simu, "Svm", plotMesh=False, nodeValues=False)
-
-        tic.Tac("Display", "Results", plotResult)
+    print(simu)
+    Display.Plot_Result(simu, "uy", deformFactor=factorDef, nodeValues=False)
+    Display.Plot_Result(simu, "Svm", plotMesh=False, nodeValues=False)
 
     plt.show()
