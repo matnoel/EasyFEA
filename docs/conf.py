60d9a4f9ebf74a2e6bf72ec5d2f0e32fe021eb73
matnoel <matthieu.noel7@gmail.com>
2025-09-11 14:29:01 +0200

    Update parts path in docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,19 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import re
-import sys
 
 import pyvista
 from pyvista.plotting.utilities.sphinx_gallery import DynamicScraper
-
-sys.path.append(os.path.abspath(".."))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -44,7 +36,6 @@ extensions = [
     "pyvista.ext.viewer_directive",
 ]
 autodoc_typehints = "signature"
-
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
