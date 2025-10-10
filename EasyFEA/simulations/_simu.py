3a9fa5b423f35cb78b9a0c2f3508ea717e9aab45
matnoel <matthieu.noel7@gmail.com>
2025-10-10 11:50:43 +0200

    #24 Update the `_Simu` docstrings.

--- EasyFEA/simulations/_simu.py
+++ EasyFEA/simulations/_simu.py
@@ -53,7 +53,7 @@ class _Simu(_IObserver, ABC):
     The ThermalSimuclass is relatively simple and can serve as a good starting point.\n
     See `simulations/_thermal.py` for more details.
 
-    To use the interface/inheritance, 14 methods need to be defined.
+    To use the interface/inheritance, 13 methods need to be defined.
 
     General:
     --------
