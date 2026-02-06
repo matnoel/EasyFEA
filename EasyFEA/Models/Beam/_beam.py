0b148d090b1d54974c220a45edf4bb187e562dc6
matnoel <matthieu.noel7@gmail.com>
2026-02-06 17:28:48 +0100

    Update Copyright

--- EasyFEA/Models/Beam/_beam.py
+++ EasyFEA/Models/Beam/_beam.py
@@ -1,4 +1,5 @@
-# Copyright (C) 2021-2025 Université Gustave Eiffel.
+# Copyright (C) 2021-2024 Université Gustave Eiffel.
+# Copyright (C) 2025-2026 Université Gustave Eiffel, INRIA.
 # This file is part of the EasyFEA project.
 # EasyFEA is distributed under the terms of the GNU General Public License v3, see LICENSE.txt and CREDITS.md for more information.
 
