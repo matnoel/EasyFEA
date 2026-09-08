2932ab4363ce21b775dc54fa4fab959ab6ad2eab
matnoel <noel.mat@proton.me>
2026-09-08 10:23:29 +0200

    Export ProblemType from Simulations

--- EasyFEA/Simulations/__init__.py
+++ EasyFEA/Simulations/__init__.py
@@ -5,6 +5,7 @@
 
 """This module contains all available simulation classes."""
 
+from ._problem_type import ProblemType
 from ._simu import _Simu, Load_Simu
 from ._terms import Term, Fold_terms
 from ._utils import Save_pickle, Load_pickle
